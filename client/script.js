5ffa639c50163a0c25e264a378dff0196c5247af
OlehDuts <oleh.duts@gmail.com>
2016-03-12 23:08:39 +0200

    first commit

---
+++ client/script.js
@@ -0,0 +1,14 @@
+Template.Images.helpers({
+  images: function (limit) {
+   var array = [];
+    for (var i = 0; i < limit; i++) {
+      array.push({
+        _id: i + 1,
+        src: 'img/RC_' + _.random(1,6)+ '.jpg',
+        title: 'car #' + (i + 1)
+        });
+    } 
+    return array;
+  }
+
+});
